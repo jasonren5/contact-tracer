c50369d359f6bd2b1bd6e809d5bde62ab6c7b2db
jasonren5 <jasonren72@gmail.com>
2020-10-10 00:19:45 -0500

    created firebase config file utilizing VSCode environment variables

---
+++ app/src/firebase/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_BASEURL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.auth();
+
+export default firebaseConfig;
