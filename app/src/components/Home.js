316e176047cd1b2b858194d99adc61c57e45691f
jasonren5 <jasonren72@gmail.com>
2020-10-11 18:24:30 -0500

    implemented dirty logout button

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from 'react'
+import firebase from 'firebase'
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        firebase.auth().signOut();
+    }
+
     render() {
         return (
-            <h1>you are logged in</h1>
+            <div>
+                <h1>you are logged in with user {this.props.user.email}</h1>
+                <button type="button" onClick={this.handleLogout}>Logout</button>
+            </div>
         )
     }
 }
