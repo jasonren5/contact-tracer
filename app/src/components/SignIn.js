55deea9466c3763d988778e9295afcdde5da85ab
jasonren5 <jasonren72@gmail.com>
2020-10-18 15:26:45 -0500

    created second form to handle registering, as registering now requires additional information

--- app/src/components/SignIn.js
+++ app/src/components/SignIn.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import firebase from 'firebase'
-
+import firebase from 'firebase';
 
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            password: '',
+            username: '',
+            password: ''
         };
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
         this.handleRegister = this.handleRegister.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     //login on form submit
-    handleSubmit(event) {
+    handleLogin(event) {
         console.log("login detected");
         firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
             var errorCode = error.code;
@@ -27,19 +27,21 @@ class SignIn extends React.Component {
     }
 
 
-    //from register button
+
+    //register form submission
     handleRegister(event) {
         console.log("register detected");
+
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log("register error " + errorCode + ": " + errorMessage);
-            // ...
         });
+        event.preventDefault();
     }
 
-    //set state variables accordingly if value is changed in form
+    //set state variables accordingly if value is changed in forms
     handleInputChange(event) {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
@@ -54,15 +56,27 @@ class SignIn extends React.Component {
         return (
             <div>
                 <h1>sign in</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleLogin}>
                     <label>email:
-                        <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
+                            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
                     </label>
                     <label>password:
-                        <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+                            <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
                     </label>
-                    <input type="submit" name="login-button" value="Login" />
-                    <button type="button" onClick={this.handleRegister}>Register</button>
+                    <input type="submit" name="login-button" value="login" />
+                </form>
+                <h1>or create an account</h1>
+                <form onSubmit={this.handleRegister}>
+                    <label>email:
+                            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
+                    </label>
+                    <label>username:
+                            <input type="text" name="username" value={this.state.username} onChange={this.handleInputChange} />
+                    </label>
+                    <label>password:
+                            <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+                    </label>
+                    <input type="submit" name="register-button" value="register" />
                 </form>
             </div>
         );
