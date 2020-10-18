7cf02b683ee12889637d895cd15bbc1623239aca
jasonren5 <jasonren72@gmail.com>
2020-10-18 16:14:53 -0500

    added validation for login and registration

--- app/src/components/SignIn.js
+++ app/src/components/SignIn.js
@@ -7,39 +7,95 @@ class SignIn extends React.Component {
         this.state = {
             email: '',
             username: '',
-            password: ''
+            password: '',
+            loginError: '',
+            registerError: ''
         };
 
         this.handleLogin = this.handleLogin.bind(this);
+        this.validateLogin = this.validateLogin.bind(this);
         this.handleRegister = this.handleRegister.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.validateRegister = this.validateRegister.bind(this);
     }
 
     //login on form submit
-    handleLogin(event) {
+    async handleLogin(event) {
         console.log("login detected");
-        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("login error " + errorCode + ": " + errorMessage);
-        });
         event.preventDefault();
+
+        if (this.validateLogin()) {
+            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(error => {
+                this.setLoginError(error.message);
+                console.error(error);
+            });
+        }
     }
 
+    //validates the login field
+    validateLogin() {
+        if (this.state.email.length < 1) {
+            this.setLoginError("Email cannot be empty");
+            return false;
+        }
 
+        if (this.state.password.length < 1) {
+            this.setLoginError("Password cannot be empty");
+            return false;
+        }
+
+        return true;
+    }
+
+    //sets loginError in state
+    //errorMessage: string containing error message
+    setLoginError(errorMessage) {
+        this.setState({
+            loginError: "Error: " + errorMessage
+        });
+    }
 
     //register form submission
     handleRegister(event) {
-        console.log("register detected");
+        if (this.validateRegister()) {
+            firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(error => {
+                // Handle Errors here.
+                this.setRegisterError(error.message);
+                console.error(error);
+            });
+        }
 
-        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("register error " + errorCode + ": " + errorMessage);
-        });
         event.preventDefault();
     }
+
+    //validate the registration form. return true if valid, false otherwise
+    validateRegister() {
+        if (this.state.email.length < 1) {
+            this.setRegisterError("Email cannot be empty.");
+            return false;
+        }
+
+        if (this.state.username.length < 1) {
+            this.setRegisterError("Username cannot be empty.")
+            return false;
+        }
+
+        if (this.state.password.length < 1) {
+            this.setRegisterError("Password cannot be empty.");
+            return false;
+        }
+
+        return true;
+    }
+
+    //sets registerError in state
+    //errorMessage: string containing error message
+    setRegisterError(errorMessage) {
+        this.setState({
+            registerError: "Error: " + errorMessage
+        });
+    }
+
 
     //set state variables accordingly if value is changed in forms
     handleInputChange(event) {
@@ -56,6 +112,7 @@ class SignIn extends React.Component {
         return (
             <div>
                 <h1>sign in</h1>
+                <p class="error">{this.state.loginError}</p>
                 <form onSubmit={this.handleLogin}>
                     <label>email:
                             <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
@@ -65,7 +122,9 @@ class SignIn extends React.Component {
                     </label>
                     <input type="submit" name="login-button" value="login" />
                 </form>
+
                 <h1>or create an account</h1>
+                <p class="error">{this.state.registerError}</p>
                 <form onSubmit={this.handleRegister}>
                     <label>email:
                             <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
