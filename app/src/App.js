63f9ade5bf756c0c7970e44ad799a88bec251a82
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-10-19 16:29:09 -0500

    delete bloatware

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/SignIn.js';
 import firebaseConfig from './firebase/config.js'
