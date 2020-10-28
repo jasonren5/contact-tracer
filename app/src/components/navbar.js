0da81ab9e797bff957138896d6d665a100c31cc0
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-10-28 16:01:04 -0500

    added material inports to navbar

--- app/src/components/navbar.js
+++ app/src/components/navbar.js
@@ -1,32 +1,40 @@
 import React from 'react'
 import firebase from 'firebase';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  MenuIcon,
+  Typography,
+  Button
+} from '@material-ui/core';
 
-class navbar extends React.Component{
-touchnavbar(){
-  firebase.auth().signOut()
-  .then(function() {
-    // Sign-out successful.
-  })
-  .catch(function(error) {
-    // An error happened
-  });
+class navbar extends React.Component {
+  touchnavbar() {
+    firebase.auth().signOut()
+      .then(function () {
+        // Sign-out successful.
+      })
+      .catch(function (error) {
+        // An error happened
+      });
 
-}
-    render() {
-        return(
-<AppBar position="static">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      News
+  }
+  render() {
+    return (
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
     </Typography>
-    <Button color="inherit" onPress = {this.touchnavbar} href= "/">logout;</Button>
-    <Button color="inherit" href= "/">home</Button>
-  </Toolbar>
-</AppBar>
-        )
+          <Button color="inherit" onPress={this.touchnavbar} href="/">logout;</Button>
+          <Button color="inherit" href="/">home</Button>
+        </Toolbar>
+      </AppBar>
+    )
 
-    }
+  }
 }
