4f5e1b85973678662d97b5b7ef44806e9886d26e
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-11-02 11:36:02 -0600

    created the util for the new function

--- app/src/utils/functions/articles.js
+++ app/src/utils/functions/articles.js
@@ -7,22 +7,33 @@ require("firebase/functions");
 var functions = firebase.functions();
 
 async function getFullArticle(article_id) {
-    
+
     var getFullArticleByID = functions.httpsCallable("getFullArticleByID");
 
-    var articlePromise = getFullArticleByID({article_id: article_id}).then((response) => {
+    var articlePromise = getFullArticleByID({ article_id: article_id }).then((response) => {
         let data = response.data;
         var sections = [];
         data.section_data.forEach((section) => {
-            var s = new ArticleSection(data.article_data.article_id,section.section_id,section.type,section.body,[])
+            var s = new ArticleSection(data.article_data.article_id, section.section_id, section.type, section.body, [])
             sections.push(s)
-        })
+        });
 
         var article = new Article(data.article_data.article_id, data.article_data.title, data.article_data.image_url, "Hello, World!", sections);
         return article;
-    })
+    });
 
     return articlePromise;
-}
+};
 
-export {getFullArticle};
+async function getAllArticles() {
+    var getAllArticles = functions.httpsCallable("getAllArticles");
+
+    var articlesPromise = getAllArticles().then((response) => {
+        let data = response.data;
+        return data;
+    });
+
+    return articlesPromise;
+};
+
+export { getFullArticle, getAllArticles };
