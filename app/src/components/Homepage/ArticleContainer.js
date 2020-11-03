912a77c7b2fd6014de560b53b9b3fabd28f8906d
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-11-02 18:21:58 -0600

    shitty card version

--- app/src/components/Homepage/ArticleContainer.js
+++ app/src/components/Homepage/ArticleContainer.js
@@ -1,15 +1,93 @@
 import React from 'react';
 
+import {
+    Card,
+    Grid,
+    CardMedia,
+    CardContent,
+    Typography,
+    Link,
+    Button
+} from '@material-ui/core';
+
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    root: {
+        display: 'flex',
+        textAlign: 'center',
+        // height: 250
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+        // width: 400,
+    },
+    image: {
+        width: 300,
+        height: 200
+    },
+    button: {
+        margin: theme.spacing(1),
+        width: 150
+    },
+    buttonHolder: {
+        textAlign: 'right'
+    },
+    contentSnipet: {
+        textAlign: 'left'
+    }
+}));
+
 export default function ArticleContainer(props) {
     const articleURL = "/article/" + props.article.id;
 
+    const classes = useStyles();
+
     return (
-        <div className="Article Container">
-            <a href={articleURL}>
-                <img src={props.article.image_url} alt={props.article.title} />
-                <p>{props.article.title}</p>
-                <p>{props.article.id}</p>
-            </a>
-        </div>
+        // <Box className="Article Container">
+        <Grid item xs={"auto"}>
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.image}
+                    image={props.article.image_url}
+                    title={props.article.title}
+                />
+                <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                        <Link href={articleURL} color={"primary"}>
+                            <Typography component="h5" variant="h5" gutterBottom>{props.article.title}</Typography>
+                        </Link>
+                        <Typography className={classes.contentSnipet} variant="body1" color="textSecondary">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate et veritatis tenetur dicta corrupti, accusantium dolorem natus non vitae ratione quaerat dignissimos aliquid unde maiores accusamus fugiat enim, numquam laborum.
+                            </Typography>
+                    </CardContent>
+                    <div className={classes.buttonHolder}>
+                        <Button
+                            // variant="contained"
+                            color="secondary"
+                            className={classes.button}
+                            endIcon={<NavigateNextIcon />}
+                        >
+                            Read More
+                    </Button>
+                    </div>
+                </div>
+            </Card>
+        </Grid>
+        // </Box>
     );
 }
--- app/src/components/Homepage/ArticleList.js
+++ app/src/components/Homepage/ArticleList.js
@@ -2,7 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { getAllArticles } from '../../utils/functions/articles';
 import ArticleContainer from './ArticleContainer';
 
+import { Container, Grid, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useMediaQuery } from 'react-responsive'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    }
+}));
+
 export default function ArticleList() {
+    const classes = useStyles();
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+
     const INITIAL_STATE = {
         articles: []
     };
@@ -19,11 +32,23 @@ export default function ArticleList() {
     }, []);
 
     return (
-        <div className="Articles List">
-            {state.articles.length > 0 && state.articles.map(article => {
-                return <ArticleContainer key={article.id} article={article} />;
-            })
-            }
-        </div>
+
+        <Container component="main" className="Article List" maxWidth={isTabletOrMobile ? 'xs' : "lg"}>
+            <CssBaseline />
+            <div className={classes.root}>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    spacing={4}
+                >
+                    {state.articles.length > 0 && state.articles.map(article => {
+                        return <ArticleContainer key={article.id} article={article} />;
+                    })
+                    }
+                </Grid>
+            </div>
+        </Container>
     );
 }
