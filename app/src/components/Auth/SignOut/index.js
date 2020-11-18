c153d3e88df0884292689327ee4b336aadb55b1b
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-11-18 01:23:32 -0600

    made sign out button more modular

--- app/src/components/Auth/SignOut/index.js
+++ app/src/components/Auth/SignOut/index.js
@@ -2,13 +2,13 @@ import React, { useContext } from 'react';
 import { Button } from '@material-ui/core';
 import { FirebaseContext } from '../../../utils/firebase';
 
-export default function SignOutButton() {
+export default function SignOutButton(props) {
     const firebase = useContext(FirebaseContext);
 
     return (
         <Button
-            variant="contained"
-            color="secondary"
+            {...props}
+            href="/"
             onClick={firebase.doSignOut}
         >
             Sign Out
