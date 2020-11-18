4205ff45f9d967634ed5b87aaec0ae8330d426bb
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-11-18 01:23:45 -0600

    added functioning signout and sign in to navbar

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-// import firebase from 'firebase';
+import React, { useState, useContext } from 'react';
+import { FirebaseContext } from '../../utils/firebase';
+
 import {
     AppBar,
     Toolbar,
@@ -13,6 +14,8 @@ import { Home } from "@material-ui/icons"
 
 import CreateArticleModal from './CreateArticleModal';
 import { makeStyles } from '@material-ui/core/styles';
+import SignOutButton from '../Auth/SignOut';
+
 
 const useStyles = makeStyles((theme) => ({
     title: {
@@ -28,19 +31,14 @@ const useStyles = makeStyles((theme) => ({
 export default function Navbar() {
     const [newArticleIsOpen, setnewArticleIsOpen] = useState(false);
     const classes = useStyles();
+    const firebase = useContext(FirebaseContext);
+    const currentUser = firebase.auth.currentUser;
 
-    // TODO: Import the signout button i already made in auth components
-    // TODO: Remeber that signin should also sign the user out
-    // const handleLogout = () => {
-    //     auth.signOut()
-    //         .then(function () {
-    //             // Sign-out successful.
-    //         })
-    //         .catch(function (error) {
-    //             // An error happened
-    //             console.log(error);
-    //         });
-    // };
+    console.log(currentUser);
+
+    const handleSignOut = () => {
+        firebase.doSignOut();
+    }
 
     const openNewArticleModal = () => {
         setnewArticleIsOpen(true);
@@ -65,9 +63,9 @@ export default function Navbar() {
                     {/* <IconButton edge="start" color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton> */}
-                    <Button color="inherit" href="/">home</Button>
-                    <Button color="inherit" href="/">logout</Button>
-                    <Button color="inherit" href="/signin">sign in</Button>
+                    <Button color="inherit" href="/">Home</Button>
+                    <Button color="inherit" onClick={handleSignOut} href="/" >Sign Out</Button>
+                    <Button color="inherit" onClick={handleSignOut} href="/signin">Sign In</Button>
                     <Button color="inherit" onClick={openNewArticleModal}>create blank article</Button>
                 </Toolbar>
             </AppBar>
