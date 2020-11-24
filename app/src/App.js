b4676004f55a0d63f81ef9b395e28af4fc4a0e8a
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-11-24 15:09:32 -0600

    added browse pages to router

--- app/src/App.js
+++ app/src/App.js
@@ -11,12 +11,15 @@ import SignInPage from './pages/Auth2.0/SignIn';
 import SignUpPage from './pages/Auth2.0/SignUp';
 import PasswordForgetPage from './pages/Auth2.0/PasswordForget';
 import HomePage from './pages/HomePage';
+import ContributeBrowse from './pages/Browse/ContributeBrowse'
+import PublishedBrowse from './pages/Browse/PublishedBrowse'
 import PageNotFound from './pages/PageNotFound.js';
 import ArticlePage from './pages/Article/ArticlePage';
 import EditArticlePage from './pages/Article/EditArticlePage';
 import PublicProfile from './pages/Profile/PublicProfile';
 import PrivateProfile from './pages/Profile/PrivateProfile';
 import Navbar from './components/Navbar/Navbar';
+import Loading from './components/Loading/PageLoading'
 
 import { withAuthentication } from './utils/session';
 
@@ -29,6 +32,8 @@ const App = () => (
         <Route path={'/signin'} component={SignInPage} />
         <Route path={'/signup'} component={SignUpPage} />
         <Route path={'/forgot-password'} component={PasswordForgetPage} />
+        <Route path={'/browse/contribute'} component={ContributeBrowse} />
+        <Route path={'/browse/published'} component={PublishedBrowse} />
         <Route path={'/article/:articleId'} component={ArticlePage} />
         <Route path={'/contribute/:articleId'} component={EditArticlePage} />
         <Route path={'/user/:userId'} component={PublicProfile} />
