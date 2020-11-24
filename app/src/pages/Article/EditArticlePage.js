cabdd295d13bc98811ada43abf02f82250a60625
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-11-24 17:33:59 -0600

    todo note

--- app/src/pages/Article/EditArticlePage.js
+++ app/src/pages/Article/EditArticlePage.js
@@ -57,6 +57,7 @@ class EditArticlePage extends React.Component {
         // Render the article
         // TODO: Utilize hover effects on each of the section parts (make it just like the ArticleSection until hovered over)
         // TODO: Loading animation for when someone creates/edits a section and it hasn't updated yet
+        //TODO: Once this is done, we don't need create article with a blank section
         return (
             <Container
                 component="main"
