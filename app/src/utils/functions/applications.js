3cdebe187eb529f6b1e273d5689dadc738e7a0e0
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-12-10 00:36:37 -0600

    added utils for app functions

--- app/src/utils/functions/applications.js
+++ app/src/utils/functions/applications.js
@@ -1,9 +1,12 @@
 async function submitApplication(firebase, type, body) {
     var submitApplication = firebase.functions.httpsCallable("applyForMod");
 
+    const name = (firebase.doGetCurrentName() ? firebase.doGetCurrentName() : "Anonymous User");
+
     const data = {
         type: type,
-        body: body
+        body: body,
+        name: name
     }
 
     try {
@@ -17,6 +20,56 @@ async function submitApplication(firebase, type, body) {
     }
 }
 
+async function getUserApplications(firebase) {
+    var getApplications = firebase.functions.httpsCallable("getUserApplications");
+
+    try {
+        var response = await getApplications();
+        return response.data
+    }
+    catch{
+        return {
+            error: "Error getting applications."
+        }
+    }
+}
+
+async function getPendingApplications(firebase) {
+    var getApplications = firebase.functions.httpsCallable("getPendingApplications");
+
+    try {
+        var response = await getApplications();
+        console.log(response);
+        return response.data
+    }
+    catch{
+        return {
+            error: "Error getting applications."
+        }
+    }
+}
+
+async function getApplication(firebase, application_id) {
+    var getApplication = firebase.functions.httpsCallable("getApplicationById");
+
+    const data = {
+        application_id: application_id,
+    }
+
+    try {
+        var response = await getApplication(data);
+        return response.data
+    }
+    catch{
+        return {
+            error: "Error getting applications."
+        }
+    }
+}
+
 export {
-    submitApplication
+    submitApplication,
+    getUserApplications,
+    getPendingApplications,
+    getApplication
 }
