2e4b839838e4c28cb9d69665c39305e4dddae901
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-12-14 02:19:41 -0600

    set up handle functions for editing and deleting source

--- app/src/components/Sources/EditSourceItem.js
+++ app/src/components/Sources/EditSourceItem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { Link, ListItem, Typography, ListItemIcon, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,6 +7,7 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import { FirebaseContext } from '../../utils/firebase';
+import ConfirmModal from '../Modals/ConfirmModal';
 
 const useStyles = makeStyles((theme) => ({
     button: {
@@ -18,27 +19,58 @@ const useStyles = makeStyles((theme) => ({
 export default function SourcesList(props) {
     const classes = useStyles();
     const firebase = useContext(FirebaseContext);
+    const [removeModal, setRemoveModal] = useState(false);
+    const [publishingDelete, setPublishingDelete] = useState(false);
+
+    // const handleScroll = () => {
+    //     document.getElementById("title").scrollIntoView()
+    // };
+
+    const handleDeleteSource = () => {
+
+    }
+
+    const handleEditSource = () => {
+
+    }
+
+    const handleOpenRemoveModal = () => {
+        setRemoveModal(true);
+        setPublishingDelete(false);
+    }
+
+    const handleCloseRemoveModal = () => {
+        setRemoveModal(false);
+        setPublishingDelete(false);
+    }
 
     return (
-        <ListItem>
-            <Typography noWrap>
-                <Link href={"#" + props.source.section_id}>
+        <div className="root">
+            <ListItem>
+                <Typography noWrap>
                     {props.source.order + ": "}
-                </Link>
-                <Link href={props.source.url}>
-                    {props.source.url}
-                </Link>
-            </Typography>
-            {(firebase.auth.currentUser.uid === props.source.user) &&
-                <ListItemIcon>
-                    <IconButton className={classes.button}>
-                        <EditIcon color="secondary" />
-                    </IconButton>
-                    <IconButton className={classes.button}>
-                        <DeleteIcon color="secondary" />
-                    </IconButton>
-                </ListItemIcon>
-            }
-        </ListItem>
+                    <Link href={props.source.url}>
+                        {props.source.url}
+                    </Link>
+                </Typography>
+                {(firebase.auth.currentUser.uid === props.source.user_id) &&
+                    <ListItemIcon>
+                        <IconButton onClick={handleEditSource} className={classes.button}>
+                            <EditIcon color="secondary" />
+                        </IconButton>
+                        <IconButton onClick={handleOpenRemoveModal} className={classes.button}>
+                            <DeleteIcon color="secondary" />
+                        </IconButton>
+                    </ListItemIcon>
+                }
+            </ListItem>
+            <ConfirmModal
+                open={removeModal}
+                closeModal={handleCloseRemoveModal}
+                handleConfirm={handleDeleteSource}
+                confirmAction={"Delete Source"}
+                publishingConfirm={publishingDelete}
+            />
+        </div>
     )
 }
