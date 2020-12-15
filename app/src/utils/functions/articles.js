d4aef10c11319977f9ea28c7c8d1de9c75375d4a
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-12-14 19:41:35 -0600

    final merge fix

--- app/src/utils/functions/articles.js
+++ app/src/utils/functions/articles.js
@@ -206,6 +206,7 @@ async function editSource(firebase, article_id, source_id, new_url) {
 
     return response;
 }
+
 async function getSectionByID(firebase, article_id, section_id) {
     let getSection = firebase.functions.httpsCallable("getSectionByID");
 
