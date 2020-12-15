ace2f98d1bdc7d9961f712f7f3ce151adb62f0ce
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-12-15 17:06:50 -0600

    last updated

---
+++ app/src/components/Articles/LastUpdated.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    text: {
+        padding: ".5rem",
+        marginTop: ".1rem",
+    }
+}));
+
+export default function LastUpdated(props) {
+    const classes = useStyles();
+
+    return (
+        <Typography color="secondary" variant="h6" className={classes.text}>
+            Last Updated: {props.updated}
+        </Typography>
+    );
+}
