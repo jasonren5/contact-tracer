af0e1b6405aea0f8d66f2dbc5d738eaf357110b1
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2021-03-31 17:42:19 -0500

    get banned words from firebase

--- app/src/utils/filter/filter.js
+++ app/src/utils/filter/filter.js
@@ -1,6 +1,11 @@
 /* File to add any words that we feel need to be banned or any words we don't want to be banned. Eventually we could probaly host this is */
 var filter = require('leo-profanity');
+import axios from 'axios';
 
-filter.add(['b00b', 'fucks']);
+axios.get('https://us-central1-cse437project.cloudfunctions.net/getFilterSettings').then(res => {
+    console.log(res.data);
+    filter.add(res.data.banned);
+    filter.remove(res.data.whitelisted);
+});
 
 export default filter;
