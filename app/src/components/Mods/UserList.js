b1a5b08162ba27c8f62b47f668982e3c6aedf433
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2021-04-22 16:31:42 -0500

    created filter menu

--- app/src/components/Mods/UserList.js
+++ app/src/components/Mods/UserList.js
@@ -4,12 +4,18 @@ import {
     Typography,
     Paper,
     List,
+    IconButton,
+    Menu,
+    MenuItem,
+    ListItemIcon,
+    ListItemText
 } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { FirebaseContext } from '../../utils/firebase';
 import { getUserList, banUser, unbanUser } from '../../utils/functions/users';
 import PersonListing from './UserListing';
+import { Block, FilterList, Gavel, LockOpen, VerifiedUser } from '@material-ui/icons';
 
 const useStyles = makeStyles(() => ({
     body: {
@@ -21,13 +27,26 @@ const useStyles = makeStyles(() => ({
     title: {
         fontWeight: "bold"
     },
+    editButtonWrapper: {
+        position: "absolute",
+        top: 0,
+        right: 40,
+    },
+    wrapper: {
+        position: "relative",
+    }
 }));
 
 export default function UserList() {
     const classes = useStyles();
     const firebase = useContext(FirebaseContext);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
     const [userList, setUserList] = useState([]);
+
+    // TODO: Filter by admin, banned, unbanned, and mod. Maybe sort by contributions
+
+    // TODO: Mobile friendly
 
     useEffect(() => {
         getUserList(firebase).then((users) => {
@@ -83,10 +102,60 @@ export default function UserList() {
         })
     }
 
+    const handleOpenMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <div className="banned-people-view">
+        <div className={classes.wrapper}>
             <Paper className={classes.body}>
                 <Typography variant="h4" className={classes.title}>User List</Typography>
+                <div className={classes.editButtonWrapper} >
+                    <IconButton
+                        onClick={handleOpenMenu}
+                        aria-label="filter-users"
+                        color="primary"
+                    >
+                        <FilterList />
+                    </IconButton>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleCloseMenu}
+                    >
+                        <MenuItem disabled >Filter Users By:</MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <ListItemIcon>
+                                <Block fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Banned" />
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <ListItemIcon>
+                                <LockOpen fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Unbanned" />
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <ListItemIcon>
+                                <VerifiedUser fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Admins" />
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseMenu}>
+                            <ListItemIcon>
+                                <Gavel fontSize="small" />
+                            </ListItemIcon>
+                            <ListItemText primary="Moderators" />
+                        </MenuItem>
+                    </Menu>
+                </div>
                 <List>
                     {userList.map((user) =>
                         <PersonListing
